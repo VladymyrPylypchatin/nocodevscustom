a9cada22ecbf6dafd7f9da09b39561af74bbaaf4
Vladimir Pilipchatin <vladymyr.pylypchatin@gmail.com>
2020-07-13 11:21:22 +0300

    analytics

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,18 @@
 import styles from './Footer.module.scss';
+import TagManager from 'react-gtm-module';
 
 const Footer = () => {
 
     return (
         <footer className={styles.footer}>
             <div className={styles.footer__madeBy}>
-                made by <a href="https://twitter.com/VPilipchatin" target="_blank">Vova Pilipchatin</a> a digital product engineer
+                made by <a onClick={() => {
+                    TagManager.dataLayer({
+                        dataLayer: {
+                            event: 'OpenMaker',
+                        }
+                    })
+                }} href="https://twitter.com/VPilipchatin" target="_blank">Vova Pilipchatin</a> a digital product engineer
             </div>
         </footer>
     );
